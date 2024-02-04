94cf35b67ed96eb310ebe428aaa03e61b4c032ed
Iman Abbasi <ilvlanabbasi@gmail.com>
2024-02-04 22:45:07 +0330

    update config

--- config/laravel-encrypter.php
+++ config/laravel-encrypter.php
@@ -1,7 +1,11 @@
 <?php
 
 return [
-    'source'      => ['app/Admin/routes.php', 'app/Admin/bootstrap.php'], // Path(s) to encrypt
+    'source'      => [
+        'routes',
+        'app/Admin/routes.php',
+        'app/Admin/bootstrap.php'
+    ], // Path(s) to encrypt
     'destination' => 'encrypted', // Destination path
     'keep' => 'keep',
 ];
